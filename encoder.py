f9dcdef55646fda4f10f13059fd417f3ba0670ab
dqie.ee <103836441+quIiIettt@users.noreply.github.com>
2024-04-22 04:09:22 +0300

    upd

--- encoder.py
+++ encoder.py
@@ -1,14 +1,14 @@
 import hashlib
 
-def sha3_224_hash(data):
-    hash_object = hashlib.sha3_224(data.encode())
+def sha3_256_hash(data):
+    hash_object = hashlib.sha3_256(data.encode())
     hex_dig = hash_object.hexdigest()
     return hex_dig
 
 with open("input.txt", "r", encoding="utf-8") as file:
     message = file.read()
 
-hashed_message = sha3_224_hash(message)
+hashed_message = sha3_256_hash(message)
 
 with open("encoded.txt", "w", encoding="utf-8") as file:
     file.write(hashed_message)
